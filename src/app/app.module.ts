524d586b36c34e7934a2b1dbb33d96170548402a
Vitor Bertti <berttivitor@gmail.com>
2020-09-01 10:19:02 -0300

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SectionComponent } from './section/section.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SectionComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SectionComponent],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { faDropbox } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
@@ -10,8 +12,19 @@ export class SectionComponent implements OnInit {
   @Input() title = '';
   @Input() description = '';
   @Input() first = '';
+  faDropbox = faDropbox;
+  buttonVariant: number;
+  buttonText = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.buttonVariant = Math.round(Math.random());
+
+    if (this.buttonVariant === 0) {
+      this.buttonText = 'Acessar';
+    } else {
+      this.buttonText = 'Interagir';
+    }
+  }
 }
