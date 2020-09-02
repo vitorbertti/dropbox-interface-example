22bf1c4d72a582249fd2c7d8b06d057e074b649c
Vitor Bertti <berttivitor@gmail.com>
2020-09-02 16:29:26 -0300

    Created side-menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SectionComponent } from './section/section.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, SectionComponent],
+  declarations: [AppComponent, SectionComponent, SideMenuComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
