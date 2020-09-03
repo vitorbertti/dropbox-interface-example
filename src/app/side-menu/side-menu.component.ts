f8564e1b6a2bff88c86a46c0e8df3ac3e6429f28
Vitor Bertti <berttivitor@gmail.com>
2020-09-03 17:38:22 -0300

    Added side menu window event listener

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.css']
+  styleUrls: ['./side-menu.component.css'],
 })
 export class SideMenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  isActive = false;
+  scrollOpen = '';
 
-  ngOnInit(): void {
+  open = this.isActive ? 'open' : '';
+
+  onScroll() {
+    let scroll = window.scrollY;
+    this.isActive = false;
+
+    this.scrollOpen = scroll <= 300 ? 'scrollOpen' : '';
+
+    console.log(this.scrollOpen);
   }
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.onScroll);
+  }
 }
